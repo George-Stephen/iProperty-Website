96bc91862ef8c4e5686d89cb8375cc7df4d33043
George-Stephen <yazz777stevie@gmail.com>
2020-09-04 15:29:23 +0300

    added the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './components/dashboard/dashboard.component'
+import {PropertyFormComponent} from './components/property-form/property-form.component'
+import {SearchComponent} from './components/search/search.component'
+import {ContactComponent} from './components/contact/contact.component'
+import {AboutComponent} from './components/about/about.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :"",component :DashboardComponent},
+  {path :"new",component : PropertyFormComponent},
+  {path :"search",component:SearchComponent},
+  {path : "Contact",component:ContactComponent}
+  {path :"about",component : AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SearchComponent } from './components/search/search.component';
+import { PropertyFormComponent } from './components/property-form/property-form.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SearchComponent,
+    PropertyFormComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
