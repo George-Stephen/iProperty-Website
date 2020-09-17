5280a63ade85295287b0fca07b83448c2284f61e
George-Stephen <yazz777stevie@gmail.com>
2020-09-17 09:14:20 +0300

    added images

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  mylogo:string = "assets/iproperty.png";
+
   constructor() { }
 
   ngOnInit(): void {
