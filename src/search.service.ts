ce17bf1ef2c5fba1d3daaa5480bb166484cf409f
Salem Owino <salemowino18@gmail.com>
2020-09-19 14:47:40 +0300

    search functionality met

---
+++ src/search.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Property} from './property'
+import { Observable, from } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  newUser: Property;
+  postUrl:string = 'https://salemrest.herokuapp.com/api/search/?search='
+  console: any;
+
+  constructor(private http: HttpClient) {
+    this.newUser = new Property(0,"","","","",0,"",);
+}
+getUserDetail(propertyname: string) {
+  interface Property{
+    description: string;
+    name: string;
+    location: string;
+    avatar_url: string;
+    id:number;
+    price:number;
+    image:string
+  
+  }
+
+  let promise = new Promise((resolve, reject) => {
+    this.http
+      .get<Property>(
+        'https://salemrest.herokuapp.com/api/search/?search='+
+        propertyname
+      )
+      .toPromise()
+      .then(
+        (response) => {
+          this.newUser = response;
+          //console.log(this.newUser);
+          resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject(error);
+        }
+      );
+  });
+  return promise;
+}
+}
+
+
+
+  
+  
